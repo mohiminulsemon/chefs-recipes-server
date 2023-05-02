b23c919f58548cd281b7f0599983d882313eae84
mohiminulsemon <mohiminulsemon80@gmail.com>
2023-05-02 10:37:47 +0600

    created dynamic route to server for each id

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get('/information', (req, res) => {
 })
 
 
+app.get('/information/:id', (req, res) => {
+    const id = req.params.id;
+    const item = information?.find((pd)=> pd.id == id);
+    res.send({item})
+})
 
 
 app.listen(5000, () => {
