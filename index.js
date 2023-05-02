4638d20b4b1020afb68b53790ab1d8d65eb6d8da
mohiminulsemon <mohiminulsemon80@gmail.com>
2023-05-02 18:00:10 +0600

    successful at deploying

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-const express = require('express');
-const cors = require('cors');
-
+const express = require("express");
 const app = express();
 
+const cors = require("cors");
 app.use(cors());
 
-const information = require('./chefinformation.json');
+const port = process.env.PORT || 5000;
 
-app.get('/information', (req, res) => {
-    res.send(information)
-})
+const information = require("./chefinformation.json");
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-app.get('/information/:id', (req, res) => {
-    const id = req.params.id;
-    const item = information?.find((pd)=> pd.id == id);
-    res.send({item})
-})
+app.get("/information", (req, res) => {
+  res.send(information);
+});
 
+app.get("/information/:id", (req, res) => {
+  const id = req.params.id;
+  const item = information?.find((pd) => pd.id == id);
+  res.send({ item });
+});
 
-app.listen(5000, () => {
-    console.log('Server is running');
-})
+// app.listen(5000, () => {
+//     console.log('Server is running');
+// })
+
+app.listen(port, () => {
+  console.log(`Dragon API is running on port: ${port}`);
+});
+
+module.exports = app;
