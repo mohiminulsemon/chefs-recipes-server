3d8e6ac796c5b4913e6a7ea7d4bf0edbaf064adf
mohiminulsemon <mohiminulsemon80@gmail.com>
2023-05-02 10:37:09 +0600

    added chefs information

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({message: 'server is running'})
+app.use(cors());
+
+const information = require('./chefinformation.json');
+
+app.get('/information', (req, res) => {
+    res.send(information)
 })
+
+
 
 
 app.listen(5000, () => {
